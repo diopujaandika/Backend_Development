fc86bec3da2062bb554bfc171a186b2275c4c124
diopujaandika <dpujaandika17@gmail.com>
2025-06-01 08:28:54 +0700

    method/verb request

--- JavaScript/Node_JS/beginner/2_Method_Verb_Request/server.js
+++ JavaScript/Node_JS/beginner/2_Method_Verb_Request/server.js
@@ -9,18 +9,33 @@ const http = require('http')
  * @param response: objek yang digunakan untuk menanggapi permintaan
  */
 
-const requesListerner = (request, response) => {
+const requestListener = (request, response) => {   
+    //Response
     response.setHeader('Content-Type', 'text/html')
+    response.statusCode = 200
+    //response.end('<h1>Hallo HTTP Server!</h1>')
 
-    response.statusCode = 200,
-    response.end('<h1>Hallo HTTP Server!</h1>')
+    //Request
+    const {method} = request
+    if(method === 'GET'){
+        response.end('<h1>Hello Server! Your request is GET</h1>')
+    }
+    if(method === 'POST'){
+        response.end('<h1>Hello Server! Your request is POST</h1>')
+    }
+    if(method === 'PUT'){
+        response.end('<h1>Hello Server! Your request is PUT</h1>')
+    }
+    if(method === 'DELETE'){
+        response.end('<h1>Hello Server! Your request is DELETE</h1>')
+    }
 }
 
-const server = http.createServer(requesListerner)
+const server = http.createServer(requestListener)
 
+//Parameter penting yang digunakan dalam method listen() => Method untuk membuat http.server
 const port = 5000
 const host = 'localhost'
-
-server.listen(port, host, () =>{
+server.listen(port, host, () => {
     console.log(`Server berjalan pada http://${host}:${port}`)
 })
