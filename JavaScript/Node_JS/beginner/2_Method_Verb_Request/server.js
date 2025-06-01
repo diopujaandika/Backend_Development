bf76acdff504de4c99c10160866ff082182e5859
diopujaandika <dpujaandika17@gmail.com>
2025-06-01 08:04:29 +0700

    membuat http server

---
+++ JavaScript/Node_JS/beginner/2_Method_Verb_Request/server.js
@@ -0,0 +1,26 @@
+console.log('Hallo, kita akan belajar membuat server!')
+
+const http = require('http')
+
+/**
+ * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
+ * 
+ * @param request: objek yang berisikan informasi terkait permintaan
+ * @param response: objek yang digunakan untuk menanggapi permintaan
+ */
+
+const requesListerner = (request, response) => {
+    response.setHeader('Content-Type', 'text/html')
+
+    response.statusCode = 200,
+    response.end('<h1>Hallo HTTP Server!</h1>')
+}
+
+const server = http.createServer(requesListerner)
+
+const port = 5000
+const host = 'localhost'
+
+server.listen(port, host, () =>{
+    console.log(`Server berjalan pada http://${host}:${port}`)
+})
