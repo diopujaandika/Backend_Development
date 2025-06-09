891f32486be63869853934d1d4a40fae62fecf65
diopujaandika <dpujaandika17@gmail.com>
2025-06-09 13:37:50 +0700

    backend pemula

---
+++ JavaScript/Node_JS/beginner/1_pengenalan_backend/5_latihan_membuat_permintaan_HTTP.js
@@ -0,0 +1,19 @@
+/**
+ * LATIHAN: Membuat Permintaan HTTP (HTTP Request)
+ * Membuat request pada web service kedai kopi melalui cURL (Client Uniform Resource Locator)
+ * cURL adalah software berbasis commend line yang dapat melakukan transaksi data melalui beberapa protokol internet, salah satunya HTTP/S.
+ */
+
+/**
+ * LANKAH BELAJAR
+ * 1. Buka Terminal (Linux dan Mac) atau CMD (Windows)
+ * 2. Ketikan curl -X GET https://coffee-api.dicoding.dev/coffees -i
+ */
+
+/**
+ * BEDAH KODE
+ * curl : perintah untuk menggunakan program cURL
+ * -X GET : menetapkan HTTP method/verb yang digunakan
+ * http://... atau https://... : Alamat request yang dituju
+ * -i : memberikan informasi detail terhadap response yang diberikan
+ */
---
+++ JavaScript/Node_JS/beginner/1_pengenalan_backend/6_rest_web_service.js
@@ -0,0 +1,8 @@
+/**
+ * REST
+ * REST (Representational State Transfer) adalah salah satu gaya arsitektur yang dapat diadaptasi ketika membangun web service.
+ * RESTful APImerupakan sebuatan web services yang menerapkan arsitektur REST. 
+ * REST juga merupakan API (Aplication Program Interface) kerena ia digunakan untuk menjebatani antara sistem yang berbeda (client dan server)
+ * API atau Aplication Program Interface merupakan antarmuka yang menjadi perantara antara sistem aplikasi yang berbeda, bisa berbentuk Web Service, SDK (Software Development Kit) atau bentuk lainnya.
+ * 
+ */
