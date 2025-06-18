94204c33037b17857e288c8e19bd195f558cbd72
diopujaandika <dpujaandika17@gmail.com>
2025-06-18 16:34:44 +0700

    query parameters

---
+++ JavaScript/Node_JS/beginner/3_web_service/13_query_parameters/routes.js
@@ -0,0 +1,61 @@
+// server.routes({
+//     method: 'GET',
+//     path: '/',
+//     handler: (request, h) => {
+//         const {name, location} = request.query;
+//         return `Hallo, ${name}, from ${location}`;
+//     },
+// });
+
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const {name = 'Stranger'} = request.params;
+            const {lang} = request.query;
+
+            if(lang === 'id'){
+                return `Hai, ${name}!`;
+            }
+            return `Hello, ${name}!`;
+        },
+    },
+    {
+        method: '*',
+        path: '/{any}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        }
+    }
+]
+
+module.exports = routes;
