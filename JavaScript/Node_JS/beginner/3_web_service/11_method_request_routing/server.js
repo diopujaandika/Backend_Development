215b029201a7ad5578ef149f33c006a62f6f33b9
diopujaandika <dpujaandika17@gmail.com>
2025-06-18 13:41:25 +0700

    make about method/verb request and routing

--- JavaScript/Node_JS/beginner/3_web_service/11_method_request_routing/server.js
+++ JavaScript/Node_JS/beginner/3_web_service/11_method_request_routing/server.js
@@ -1,4 +1,5 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
@@ -6,14 +7,10 @@ const init = async () => {
         host: 'localhost'
     });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Hello World!'
-        }
-    });
+    server.route(routes);
 
     await server.start();
-    console.log(``)
-}
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
