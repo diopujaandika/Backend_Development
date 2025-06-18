de944e3a8724cf57a589a4cb81911c20ed321553
diopujaandika <dpujaandika17@gmail.com>
2025-06-18 16:39:58 +0700

    body/payload request

---
+++ JavaScript/Node_JS/beginner/3_web_service/14_body_payload_request/routes.js
@@ -0,0 +1,8 @@
+server.route({
+    method: 'POST',
+    path: '/login',
+    handler: (request, h) => {
+        const {username, password} = request.payload;
+        return `Welcome ${username}`
+    }
+})
