813cb9aa64d894a904965acae5acbdd1adbe5935
Dio Puja Andika <dpujaandika17@gmail.com>
2026-06-16 11:43:17 +0700

    update

--- JavaScript/Node_JS/beginner/3_web_service/3_HTTP_Server/server.js
+++ JavaScript/Node_JS/beginner/3_web_service/3_HTTP_Server/server.js
@@ -14,7 +14,17 @@ import http from 'http';
  * @param response : objek yang digunakan untuk menanggapi permintaan
  */
 const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html')
 
+    response.statusCode = 200
+    response.end('<h1> Halo HTTP Server!</h1>')
 };
 
 const server = http.createServer(requestListener) //Untuk membuat server kita mambutuhkan satu method yaitu: http.createServer(), sesuai namanya method ini berfungsi untuk membuat HTTP server.
+
+//Method listen() => method yang membuat http.server selalu standby untuk menangani permintaan yang masuk dari client. terdiri dari
+    const port = 5000                   //port(number)  : jalur yang digunakan untuk mengakses HTTP server
+    const host = 'localhost'            //host(string)  : nama domain yang digunakan oleh HTTP server
+    server.listen(port, host, () => {   //listeningListener(funtion): callback yang akan dipanggil ketika HTTP server sedang bekerja
+        console.log(`Server berjalan pada http://${host}:${port}`)
+    })
