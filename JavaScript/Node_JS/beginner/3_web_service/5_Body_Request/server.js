44aba5da6ef069c4366396f1df25e6703d67fd51
Dio Puja Andika <dpujaandika17@gmail.com>
2026-06-16 17:55:47 +0700

    update

--- JavaScript/Node_JS/beginner/3_web_service/5_Body_Request/server.js
+++ JavaScript/Node_JS/beginner/3_web_service/5_Body_Request/server.js
@@ -1,35 +1,33 @@
-const http = require('http');
- 
+import http from 'http'
+
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
- 
-    const { method } = request;
- 
-    if(method === 'GET') {
-        response.end('<h1>Hello!</h1>');
+    response.setHeader('Content-Type', 'text/html')
+    response.statusCode = 200
+
+    const {method} = request
+    if(method === 'GET'){
+        response.end ('<h1>Anda melakukan GET pada server!</h1>')
     }
- 
-    if(method === 'POST') {
-        let body = [];
-            
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        
+    if(method === 'POST'){
+        let body = []       //Variabel body dengan value array kosong berfungsi untuk menampung buffer pada stream
+
+        request.on('data', (chunk) => { 
+            body.push(chunk)
+        })
+
         request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}!</h1>`);
-        });
+            body = Buffer.concat(body).toString()
+            const {name} = JSON.parse(body) //JSON.parse() digunakan untuk mengubah JSON String menjadi JavaScript Objek
+            response.end (`<h1>Hai ${name}!, Anda melakukan POST pada server!</h1>`)
+        })
     }
-};
- 
-const server = http.createServer(requestListener);
- 
-const port = 5000;
-const host = 'localhost';
- 
-server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`);
-});
+}
+
+const server = http.createServer(requestListener)
+
+const port = 5000
+const host = 'localhost'
+
+server.listen(port, host,() => {
+    console.log(`Server sedang berjalan pada http://${host}:${port}`)
+})
