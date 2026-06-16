3f82f1a85df4aa79982f6fb663433fd4633899f2
Dio Puja Andika <dpujaandika17@gmail.com>
2026-06-16 14:22:58 +0700

    update method/verb request

--- JavaScript/Node_JS/beginner/3_web_service/4_Method_Verb_Request/server.js
+++ JavaScript/Node_JS/beginner/3_web_service/4_Method_Verb_Request/server.js
@@ -1,41 +1,30 @@
-console.log('Hallo, kita akan belajar membuat server!')
+import http from 'http'
 
-const http = require('http')
-
-/**
- * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
- * 
- * @param request: objek yang berisikan informasi terkait permintaan
- * @param response: objek yang digunakan untuk menanggapi permintaan
- */
-
-const requestListener = (request, response) => {   
-    //Response
-    response.setHeader('Content-Type', 'text/html')
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type','text/html')
     response.statusCode = 200
-    //response.end('<h1>Hallo HTTP Server!</h1>')
 
-    //Request
-    const {method} = request
+    //Membuat logika dalam menganni permintaan dari method yang berbeda.
+    //Kita akan menggunakan parameter request yang merupakan instance dari http.ClientRequest
+    const {method} = request //Properti method itu berbentuk String (GET, POST, PUT, DELETE, DLL)
     if(method === 'GET'){
-        response.end('<h1>Hello Server! Your request is GET</h1>')
+        response.end ('<h1>Anda melakukan GET pada server!</h1>')
     }
     if(method === 'POST'){
-        response.end('<h1>Hello Server! Your request is POST</h1>')
+        response.end ('<h1>Anda melakukan POST pada server!</h1>')
     }
     if(method === 'PUT'){
-        response.end('<h1>Hello Server! Your request is PUT</h1>')
+        response.end('<h1>Anda melakukan POST pada server!</h1>')
     }
     if(method === 'DELETE'){
-        response.end('<h1>Hello Server! Your request is DELETE</h1>')
+        response.end('<h1>Anda melakukan DELETE pada server!</h1>')
     }
 }
 
 const server = http.createServer(requestListener)
 
-//Parameter penting yang digunakan dalam method listen() => Method untuk membuat http.server
-const port = 5000
-const host = 'localhost'
-server.listen(port, host, () => {
+const port = 5000 
+const host = 'localhost'   
+server.listen(port, host,() => {
     console.log(`Server berjalan pada http://${host}:${port}`)
 })
