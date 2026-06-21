f96e37fda8b15e5bd35ce4134cc449213b660f85
Dio Puja Andika <dpujaandika17@gmail.com>
2026-06-21 20:04:44 +0700

    make a RESTFul API http server with express framework

---
+++ JavaScript/Node_JS/beginner/project/notes-app-back-end/src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+const host = 'localhost';
+
+app.listen(port, () => {
+    console.log(`Server running at http://${host}:${port}`);
+});
