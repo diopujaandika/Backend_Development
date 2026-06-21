0766568d9a05ce57de5e3d287580162bac0352e4
Dio Puja Andika <dpujaandika17@gmail.com>
2026-06-21 13:58:15 +0700

    make a body/payload with express

--- JavaScript/Node_JS/beginner/3_web_service/14_express_body_payload_request/server.js
+++ JavaScript/Node_JS/beginner/3_web_service/14_express_body_payload_request/server.js
@@ -5,7 +5,13 @@ const app = express();
 const host = 'localhost';
 const port = 3000;
 
-app.use('/', router);
+//Middleware untuk parsing JSON body
+app.use(express.json());
+
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    res.send(`Welcome ${username}!`);
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://${host}:${port}`); 
