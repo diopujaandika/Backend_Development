9a72adb51cb9bb234b948f08df599c63f1257ffe
Dio Puja Andika <dpujaandika17@gmail.com>
2026-06-30 23:43:00 +0700

    update node.js fundamental - Notes App Project make a middleware #1

--- JavaScript/Node_JS/fundamental/1_middleware_and_data_validation/2_latihan1_middleware/notes-app-backend-2/src/server/index.js
+++ JavaScript/Node_JS/fundamental/1_middleware_and_data_validation/2_latihan1_middleware/notes-app-backend-2/src/server/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from '../routes/index.js';
 import cors from 'cors';
+import ErrorHandler from '../middlewares/error.js';
  
 const app = express();
  
+app.use(ErrorHandler)
 app.use(express.json());
 app.use(cors({
     origin: '*'
