70a216e6099688fa6cd266003f15d9baa7a5ddf3
Dio Puja Andika <dpujaandika17@gmail.com>
2026-06-30 08:00:36 +0700

    update

--- JavaScript/Node_JS/fundamental/1_middleware_and_data_validation/2_latihan1_middleware/notes-app-backend-2/src/server.js
+++ JavaScript/Node_JS/fundamental/1_middleware_and_data_validation/2_latihan1_middleware/notes-app-backend-2/src/server.js
@@ -1,19 +1,10 @@
-import express from 'express';
-import routes from './routes.js';
-import cors from 'cors';
-
-const app = express();
-const port = process.env.PORT || 3000;
-const host = process.env.NODE_ENV !== 'production'
-    ? 'localhost'
+import server from './server/index.js';
+ 
+const host = process.env.NODE_ENV !== 'production' 
+    ? 'localhost' 
     : '0.0.0.0';
-
-app.use(express.json());
-app.use(cors({
-    origin: '*'
-}));
-app.use('/', routes);
-
-app.listen(port, () => {
-    console.log(`Server running at http://${host}:${port}`);
+const port = process.env.PORT || 3000;
+ 
+server.listen(port, () => {
+ console.log(`Server running at http://${host}:${port}`);
 });
