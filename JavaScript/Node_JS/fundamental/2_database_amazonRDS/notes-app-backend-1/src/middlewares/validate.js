af483ac0d02334fae3eb784ccc7cf7068542148e
Dio Puja Andika <dpujaandika17@gmail.com>
2026-07-02 21:18:33 +0700

    2 database amazon

---
+++ JavaScript/Node_JS/fundamental/2_database_amazonRDS/notes-app-backend-1/src/middlewares/validate.js
@@ -0,0 +1,23 @@
+const validate = (schema) => (req, res, next) => {
+    const {error, value} = schema.validate(req. body, {
+        abortEarly: false,
+        allowUnknown: false,
+        stripUnknown: true,
+    });
+
+    if (error) return next(error);
+    req.validated = value;
+    next();
+};
+
+const validateQuery = (schema) => (req, res, next) => { const {error, value} = schema.validate(req.query, {
+    abortEarly: false,
+    allowUnknown: false,
+    stripUnknown: true
+});
+if(error) return next(error);
+req.validate = value;
+next();
+};
+
+export {validate, validateQuery};
