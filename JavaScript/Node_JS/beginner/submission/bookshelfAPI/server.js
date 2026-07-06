77cb2c50a04e6340c3095e8d799ea72e61642e55
Dio Puja Andika <dpujaandika17@gmail.com>
2026-07-06 14:48:48 +0700

    Make a http server in Exercise and Submission

--- JavaScript/Node_JS/beginner/submission/bookshelfAPI/server.js
+++ JavaScript/Node_JS/beginner/submission/bookshelfAPI/server.js
@@ -1,1 +1,19 @@
 console.log(`Submission Dicoding : Kelas Backend Pemula | Bookshelf API`);
+
+import http from 'http';
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+
+    response.statusCode = 200;
+    response.end('<h1>Hallo BookshelfAPI</h1>');
+};
+
+const port = 9000;
+const host = 'localhost';
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}`);
+});
