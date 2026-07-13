c3c9e12562e989132267fd7f4d1a0cab89649cec
Dio Puja Andika <dpujaandika17@gmail.com>
2026-07-13 23:08:51 +0700

    update

--- JavaScript/Node_JS/beginner/project/nodejs-notes-app-server/src/routes.js
+++ JavaScript/Node_JS/beginner/project/nodejs-notes-app-server/src/routes.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 import {
   createNote,
+  getNotes,
 } from './controller.js';
 
 const router = express.Router();
 router.post('/notes', createNote);
+router.get('/notes', getNotes);
 
 export default router;
--- JavaScript/Node_JS/beginner/submission/bookshelfAPI/src/routes.js
+++ JavaScript/Node_JS/beginner/submission/bookshelfAPI/src/routes.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 import {
   createBook,
+  getBook,
 } from './controller.js';
 
 const router = express.Router();
 router.post('/books', createBook);
+router.get('/books', getBook);
 
 export default router;
